d617df890efcdfac92fd0f82b6817fdf747e68a2
Vincent Velociter <vincent.velociter@gmail.com>
2011-01-03 10:21:04 +0100

    Add link for gitconfig

--- symlink.sh
+++ symlink.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 ln -s ~/.dotfiles/bashrc ~/.bashrc
+ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/gvimrc ~/.gvimrc
