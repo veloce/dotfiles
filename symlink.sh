ba0854bc9c16961ac133de661fa30c5529dec226
Vincent Velociter <vincent.velociter@gmail.com>
2011-01-10 12:38:43 +0100

    Update installation script

--- symlink.sh
+++ symlink.sh
@@ -5,3 +5,5 @@ ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/gvimrc ~/.gvimrc
+ln -s ~/.dotfiles/oh-my-zsh ~/.oh-my-zsh
+ln -s ~/.dotfiles/zshrc ~/.zshrc
