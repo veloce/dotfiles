bf3c47b978ba8fb4ccdf38aee7d8f74181e9980e
Vincent Velociter <vincent.velociter@gmail.com>
2012-09-28 09:30:32 +0200

    Use urxvt as daemon

--- xmonad.hs
+++ xmonad.hs
@@ -17,7 +17,7 @@ import qualified Data.Map        as M
 -- The preferred terminal program, which is used in a binding below and by
 -- certain contrib modules.
 --
-myTerminal      = "urxvt"
+myTerminal      = "urxvtc"
 
 -- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool
