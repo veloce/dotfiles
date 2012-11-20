ce705e642a6d3b4c4bfc323ec565de96fa8a126c
Vincent Velociter <vincent.velociter@gmail.com>
2012-11-20 17:18:28 +0100

    Use yeganesh instead of dmenu

--- xmonad.hs
+++ xmonad.hs
@@ -68,7 +68,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
 
     -- launch dmenu
-    , ((modm,               xK_p     ), spawn "dmenu_run -fn Inconsolata-14")
+    , ((modm,               xK_p     ), spawn "$(yeganesh -x -- -fn Inconsolata-14) &")
 
     -- screenshot
     , ((0,                  xK_Print ), spawn "scrot")
