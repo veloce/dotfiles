8281432e2395724a24d6028fa043d5e86158c437
Vincent Velociter <vincent.velociter@gmail.com>
2012-11-24 12:02:29 +0100

    Remove unused script

--- scripts/skl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-current=`setxkbmap -print | grep xkb_symbols | awk -F"+" '{print $2}'`
-
-if [ "$current" = "us" ]; then
-    setxkbmap -layout us -variant intl
-else
-    setxkbmap -layout us
-fi
