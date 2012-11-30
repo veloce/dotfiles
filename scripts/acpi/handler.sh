47f2e833707b2322bedd5abee9e5822234fcc205
Vincent Velociter <vincent.velociter@gmail.com>
2012-11-30 11:19:26 +0100

    Add acpi handler.sh example

---
+++ scripts/acpi/handler.sh
@@ -0,0 +1,114 @@
+#!/bin/sh
+# Default acpi script that takes an entry for all actions
+
+minspeed=`cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq`
+maxspeed=`cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq`
+setspeed="/sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed"
+
+set $*
+
+case "$1" in
+    button/power)
+        case "$2" in
+            PBTN|PWRF)
+                logger "PowerButton pressed: $2"
+                poweroff
+                ;;
+            *)
+                logger "ACPI action undefined: $2"
+                ;;
+        esac
+        ;;
+    button/sleep)
+        case "$2" in
+            SLPB|SBTN)
+                echo -n mem >/sys/power/state
+                ;;
+            *)
+                logger "ACPI action undefined: $2"
+                ;;
+        esac
+        ;;
+    ac_adapter)
+        case "$2" in
+            AC|ACAD|ADP0)
+                case "$4" in
+                    00000000)
+                        echo -n $minspeed >$setspeed
+                        #/etc/laptop-mode/laptop-mode start
+                        ;;
+                    00000001)
+                        echo -n $maxspeed >$setspeed
+                        #/etc/laptop-mode/laptop-mode stop
+                        ;;
+                esac
+                ;;
+            *)
+                logger "ACPI action undefined: $2"
+                ;;
+        esac
+        ;;
+    battery)
+        case "$2" in
+            BAT0)
+                case "$4" in
+                    00000000)
+                        logger 'Battery online'
+                        ;;
+                    00000001)
+                        logger 'Battery offline'
+                        ;;
+                esac
+                ;;
+            CPU0)
+                ;;
+            *)  logger "ACPI action undefined: $2" ;;
+        esac
+        ;;
+    button/lid)
+        case "$3" in
+            close)
+                logger 'LID closed'
+                echo -n mem >/sys/power/state
+                ;;
+            open)
+                logger 'LID opened'
+                ;;
+            *)
+                logger "ACPI action undefined: $3"
+                ;;
+        esac
+        ;;
+    button/volumedown)
+        logger 'Volume down'
+        amixer set Master 2-
+        ;;
+    button/volumeup)
+        logger 'Volume up'
+        amixer set Master 2+
+        ;;
+    button/mute)
+        logger 'Mute'
+        amixer set Master toggle
+        ;;
+    cd/play)
+        logger 'Play/Pause'
+        mpc toggle
+        ;;
+    cd/next)
+        logger 'CD Next'
+        mpc next
+        ;;
+    cd/prev)
+        logger 'CD Prev'
+        mpc prev
+        ;;
+    button/wlan)
+        logger 'Rfkill switch'
+        ;;
+    *)
+        logger "ACPI group/action undefined: $1 / $2"
+        ;;
+esac
+
+# vim:set ts=4 sw=4 ft=sh et:
