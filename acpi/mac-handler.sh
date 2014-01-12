13e3a770e152723b6af83645682dc76487d07ca5
Vincent Velociter <vincent.velociter@gmail.com>
2014-01-12 12:30:02 +0100

    Control brightness

--- acpi/mac-handler.sh
+++ acpi/mac-handler.sh
@@ -6,8 +6,8 @@
 # action=/path/to/this/file.sh %e
 
 case "$1" in
-  # video/brightnessdown) /home/veloce/.dotfiles/scripts/mac-brightness down;;
-  # video/brightnessup) /home/veloce/.dotfiles/scripts/mac-brightness up;;
+  video/brightnessdown) /home/veloce/.dotfiles/scripts/mac-brightness down;;
+  video/brightnessup) /home/veloce/.dotfiles/scripts/mac-brightness up;;
   button/volumedown) amixer set Master 5-;;
   button/volumeup) amixer set Master 5+;;
   button/mute) amixer set Master toggle;;
