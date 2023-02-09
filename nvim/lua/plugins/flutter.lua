36fb723d15b6982d61123c639b69cf877f6a6ccd
Vincent Velociter <vincent.velociter@gmail.com>
2023-02-09 18:39:50 +0100

    Update flutter mappings

--- nvim/lua/plugins/flutter.lua
+++ nvim/lua/plugins/flutter.lua
@@ -17,3 +17,11 @@ require("flutter-tools").setup{
     on_attach = require("lsp-format").on_attach,
   },
 }
+
+local map = vim.keymap.set
+
+-- flutter shortcuts
+map('n', '<leader>fd', ':FlutterRun -d all')
+map('n', '<leader>fp', ':FlutterRun --dart-define=LICHESS_HOST=https://lichess.org -d all')
+map('n', '<leader>fl', ':FlutterRun --dart-define=LICHESS_HOST=http://localhost:9663 -d all')
+map('n', '<leader>ft', function() require('telescope').extensions.flutter.commands() end)
